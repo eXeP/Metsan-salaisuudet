6c2c321d428f9d8a8a12cec82f80052f64322055
Warro <olli.warro@gmail.com>
2019-11-23 12:35:13 +0200

    fixed menu

--- metsa-front/src/components/InfoContainer/index.js
+++ metsa-front/src/components/InfoContainer/index.js
@@ -6,35 +6,28 @@ const MENU_MAX_HEIGHT = 0.8
 const MENU_WIDTH = 1
 const MENU_MIN_HEIGHT = 60
 
-const Container = styled.div`
-  position: absolute;
-  height: ${MENU_MAX_HEIGHT * 100}%;
-  width: 100%;
-  bottom: 0;
-`
-
 const Menu = styled.div`
-  position: relative;
+  position: absolute;
   height: 100%;
   width: ${MENU_WIDTH * 100}%;
   background-color: #fafafa;
   bottom: 0;
-  ${({ isDragging }) => !isDragging ? 'transition: transform .4s;' : ''}
+  ${({ isDragging }) => (!isDragging ? 'transition: transform .4s;' : '')}
 `
 
 const InfoContainer = () => {
   const levels = {
     0: -1,
-    1: Math.floor(MENU_MAX_HEIGHT * window.screen.height) - 40,
-    2: Math.floor((MENU_MAX_HEIGHT / 2) * window.screen.height),
-    3: 0
+    1: window.screen.height - 40,
+    2: Math.floor((0.5) * window.screen.height),
+    3: window.screen.height - MENU_MAX_HEIGHT * window.screen.height
   }
 
   const [x, setX] = useState(Math.floor(0.5 * window.screen.width) - Math.floor((MENU_WIDTH / 2) * window.screen.width))
-  const [y, setY] = useState(Math.floor(MENU_MAX_HEIGHT * window.screen.height) - MENU_MIN_HEIGHT)
+  const [y, setY] = useState(window.screen.height - MENU_MIN_HEIGHT)
   const [isDragging, setIsDragging] = useState(false)
 
-  const bottom = Math.floor(0.8 * window.screen.height) - MENU_MIN_HEIGHT
+  const bottom = window.screen.height - MENU_MIN_HEIGHT
 
   const getNearestLevel = () => {
     let minLevel = 0
@@ -62,18 +55,16 @@ const InfoContainer = () => {
   }
 
   return (
-    <Container>
-      <Draggable
-        onStart={onStart}
-        onStop={onStop}
-        bounds={{ top: 0, bottom }}
-        axis="y"
-        onDrag={onDrag}
-        position={{ x, y }}
-      >
-        <Menu isDragging={isDragging}></Menu>
-      </Draggable>
-    </Container>
+    <Draggable
+      onStart={onStart}
+      onStop={onStop}
+      bounds={{ top: window.screen.height - MENU_MAX_HEIGHT * window.screen.height, bottom }}
+      axis="y"
+      onDrag={onDrag}
+      position={{ x, y }}
+    >
+      <Menu isDragging={isDragging}></Menu>
+    </Draggable>
   )
 }
 
