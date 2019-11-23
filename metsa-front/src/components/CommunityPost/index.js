84a4a771b37478c00f95a694917a719ce6340fa3
Pietari Kaskela <pietari.kaskela@gmail.com>
2019-11-23 16:28:52 +0200

    Post updates

--- metsa-front/src/components/CommunityPost/index.js
+++ metsa-front/src/components/CommunityPost/index.js
@@ -6,28 +6,57 @@ const PostContainer = styled.div`
   flex-direction: column;
   border: solid 1px white;
   background-color: #fafafa;
-  box-shadow: rgba(0,0,0,0.8) 0 0 10px;
+  box-shadow: rgba(0,0,0,0.8) 0 0 5px;
   border-radius: 1em;
   padding: 0em;
   margin: 0.5em;
 `
 
+const PostHeader = styled.div`
+  display: flex;
+  flex-direction: row;
+  width: 100%;
+  align-items: center;
+  padding: 0em 0em 0.5em 0em;
+`
+
 const PostTitle = styled.span`
-  padding: 0.5em;
+  padding: 0em 0.5em 0.2em 0.5em;
   font-size: 1.5em;
   font-style: bold;
 `
 
 const PostText = styled.span`
-  padding: 0.5em;
+  padding: 0em 0.5em 0.5em 0.7em;
+`
 
+const Poster = styled.div`
+  display: flex;
+  flex-direction: row-reverse;
+  align-items: center;
+  flex-grow: 1;
+`
+
+const PosterImg = styled.img`
+  border-radius: 50%;
+  width: 2em;
+  height: 2em;
+  float: left;
+  padding: 0em 1em 0em 0.5em;
 `
 
 const CommunityPost = () => {
 
   return (
     <PostContainer>
-      <PostTitle>Post test</PostTitle>
+      <PostHeader>
+        <PostTitle>Post test</PostTitle>
+        <Poster>
+          <PosterImg src='otto.jpg'></PosterImg>
+          <p>Otto L.</p>
+        </Poster>
+      </PostHeader>
+      
       <PostText>lorem ipsum</PostText>
     </PostContainer>
   )
